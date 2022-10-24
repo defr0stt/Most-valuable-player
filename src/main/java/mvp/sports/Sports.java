b9135eeb57b99367fda322896dcd85b94b169c93
defr0stt <dima.sb713@gmail.com>
2022-10-24 22:51:54 +0300

    Enum implements factory interface

--- src/main/java/mvp/sports/Sports.java
+++ src/main/java/mvp/sports/Sports.java
@@ -1,6 +1,21 @@
 package mvp.sports;
 
-public enum Sports {
-    BASKETBALL,
-    HANDBALL
+import mvp.players.BasketballPlayer;
+import mvp.players.HandballPlayer;
+import mvp.players.Player;
+import mvp.players.factory.PlayerFactory;
+
+public enum Sports implements PlayerFactory {
+    BASKETBALL {
+        @Override
+        public Player createPlayer(String name, String nickname, Integer number, String teamName, Integer... points) {
+            return new BasketballPlayer(name,nickname,number,teamName,points[0],points[1],points[2]);
+        }
+    },
+    HANDBALL {
+        @Override
+        public Player createPlayer(String name, String nickname, Integer number, String teamName, Integer... points) {
+            return new HandballPlayer(name,nickname,number,teamName,points[0],points[1]);
+        }
+    }
 }
